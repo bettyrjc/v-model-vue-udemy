c57b460ead074de87a271aa1103f134bab29fd9e
Betty <bettyjimenez3010@gmail.com>
2020-12-08 11:11:12 -0400

    initial commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <router-link to='/'>
+        formularios
+    </router-link>
+    <router-link to='/task'>
+        Tareas
+    </router-link>
+  </div>
+
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+body{
+    margin: 0;
+    background: 'black'
+
+}
+a{
+    margin: 1rem;
+    padding: 1rem;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,36 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <form class="home" @submit.prevent='procesarFormulario'>
+    <input type='text' placeholder='Ingrese nombre' v-model.trim='task.name'/>
+    <div>
+        <label for='checkbox1'>JS</label>
+        <input type='checkbox' id='checkbox1' value='js' v-model='task.categorias'/>
+    </div>
+    <div>
+        <label for='checkbox2'>NodeJS</label>
+        <input type='checkbox' id='checkbox2' value='node' v-model='task.categorias'/>
+    </div>
+
+    <div>
+      <div>
+        <label for='radio1'>Urgente</label>
+        <input type='radio' id='checkbox2' value='urgente' v-model='task.urgent'/>
+      </div>
+       <div>
+        <label for='radio2'>Relax</label>
+        <input type='radio' id='checkbox2' value='no urgente' v-model='task.urgent'/>
+      </div>
+    </div>
+    <div>
+      <input type='number' v-model.number='task.numero'/>
+    </div>
+  <p>
+    {{task}}
+  </p>
+
+  <button type='submit' :disabled='bloquear'>Procesar</button>
+  </form>
+
+
 </template>
 
 <script>
@@ -13,6 +41,35 @@ export default {
   name: 'Home',
   components: {
     HelloWorld
+  },
+  data(){
+    return{
+      task:{
+        name:'',
+        categorias:[],
+        urgent:'',
+        numero:''
+    }
   }
+},
+methods:{
+  procesarFormulario(){
+  if(this.task.name === ''){
+    console.log('vacio');
+    return
+  }
+    this.task= {
+        name:'',
+        categorias:[],
+        urgent:'',
+        numero:''
+    }
+  }
+},
+computed:{
+  bloquear(){
+    return this.task.name.trim() === "" ? true : false
+  }
+}
 }
 </script>
